a45a41d3445f6c3241c0a51f83ef877465aa26c3
prathvikshervegar <shervegarprathvik@gmail.com>
2020-10-21 17:40:42 +0530

    First commit
    uploading all files

---
+++ InternCore/src/main/java/Dlithe/InternCore/App.java
@@ -0,0 +1,13 @@
+package Dlithe.InternCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
